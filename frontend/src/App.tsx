6abf9f4b40d0e9c0655d0606f902440729b3a8a1
Rodrigo Dalla Dea <rodrigoddea@gmail.com>
2020-04-24 14:22:06 -0300

    Created signin and signup pages - Using styled components - Separated button and input as components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import GlobalStyle from './styles/global';
+
 const App: React.FC = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <SignUp />
+      <GlobalStyle />
+    </>
+  );
 };
 
 export default App;
