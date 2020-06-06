e4de9162f3c780ac11f46ff370181295fb5880be
Rodrigo Dalla Dea <rodrigoddea@gmail.com>
2020-06-06 19:20:08 -0300

    Added route and service to list providers

---
+++ backend/src/modules/appointments/services/ListProvidersService.spec.ts
@@ -0,0 +1,41 @@
+// import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import ListProvidersService from './ListProvidersService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let listProviders: ListProvidersService;
+
+describe('ListProviders', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+
+    listProviders = new ListProvidersService(fakeUsersRepository);
+  });
+
+  it('should be able to list the providers', async () => {
+    const user1 = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'john@email.com',
+      password: '6161651661',
+    });
+
+    const user2 = await fakeUsersRepository.create({
+      name: 'John Tre',
+      email: 'johntre@email.com',
+      password: '6161651661',
+    });
+
+    const loggedUser = await fakeUsersRepository.create({
+      name: 'John Qua',
+      email: 'johnqua@email.com',
+      password: '6161651661',
+    });
+
+    const providers = await listProviders.execute({
+      user_id: loggedUser.id,
+    });
+
+    expect(providers).toEqual([user1, user2]);
+  });
+});
