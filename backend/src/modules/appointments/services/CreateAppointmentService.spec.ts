781b2ddfc2f88d659524a36a06f0e97e17f4319a
Rodrigo Dalla Dea <rodrigoddea@gmail.com>
2020-06-06 23:54:08 -0300

    Modified appointment structure - Added user_id to appointment, that way we can know which user is making the appointment

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -17,6 +17,7 @@ describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
     const appointment = await createAppointment.execute({
       date: new Date(),
+      user_id: '1651684651',
       provider_id: '1651684651',
     });
 
@@ -29,12 +30,14 @@ describe('CreateAppointment', () => {
 
     await createAppointment.execute({
       date: appointmentDate,
+      user_id: '1651684651',
       provider_id: '1651684651',
     });
 
     await expect(
       createAppointment.execute({
         date: appointmentDate,
+        user_id: '1651684651',
         provider_id: '1651684651',
       }),
     ).rejects.toBeInstanceOf(AppError);
